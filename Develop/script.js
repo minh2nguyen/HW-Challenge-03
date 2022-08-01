5e92b293214c2e04acec8e22c5b3e756b0a0b5ab
MinhThu <mttnguyen91@gmail.com>
2022-08-01 18:28:18 -0500

    Added var for letters: upper and lower

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Step 1. Prompt the user for password criteria
-    // a. password length 8 < 128
-    // b. lowercase, uppercase, special characters
+
+// Password criteria for characters: numbers, symbols,& letters
 var numbers = [1,2,3,4,5,6,7,8,9,0];
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+"]
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 // At the start, generatePassword was not defined, so I defined it here: 
 function generatePassword() {
+
   // This is a confirmation to ensure me that things are connected and working 
   console.log("You clicked the button! 😀")
 
+// Step 1. Prompt the user for password criteria
+    // a. password length 8 < 128
+    // b. lowercase, uppercase, special characters
 
 // Step 2. Validate the input
 
