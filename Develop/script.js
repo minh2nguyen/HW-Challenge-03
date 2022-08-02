e589ebac5af0efdd01e41222fa21ba7e6ca9edc6
MinhThu <mttnguyen91@gmail.com>
2022-08-01 22:18:31 -0500

    Moved character criterias under function genPW

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Password criteria for characters: numbers, symbols,& letters
-var numbers = [1,2,3,4,5,6,7,8,9,0];
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
 // At the start, generatePassword was not defined, so I defined it here: 
 function generatePassword() {
-
   // This is a confirmation to ensure me that things are connected and working 
   console.log("You clicked the button! 😀")
 
 // Step 1. Prompt the user for password criteria
-    // a. password length 8 < 128
-    // b. lowercase, uppercase, special characters
+    // a. lowercase, uppercase, special characters
+    var numbers = [1,2,3,4,5,6,7,8,9,0];
+    var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "-", "_", "=", "+"];
+    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    // b. password length 8 < 128
 
 // Step 2. Validate the input
 
